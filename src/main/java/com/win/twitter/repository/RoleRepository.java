582afed1a1595cf2449222dd0afa3c2d8e6c06b9
Lisa Miller <lisayevonne@outlook.com>
2020-07-28 15:41:44 -0400

    fixed issue, boots on local server

--- src/main/java/com/win/twitter/repository/RoleRepository.java
+++ src/main/java/com/win/twitter/repository/RoleRepository.java
@@ -1,52 +1,12 @@
 package com.win.twitter.repository;
 
-import java.util.List;
-import java.util.Arrays;
-import java.util.HashSet;
-
 import com.win.twitter.model.Role;
-import com.win.twitter.model.User;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
     Role findByRole(String role);
 
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    public List<User> findAll(){
-        return (List<User>) userRepository.findAll();
-    }
-
-    public void save(User user) {
-        userRepository.save(user);
-    }
-
-    public User saveNewUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setActive(1);
-        Role userRole = roleRepository.findByRole("USER");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
-        return userRepository.save(user);
-    }
-
-    public User saveNewUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setActive(1);
-        Role userRole = roleRepository.findByRole("USER");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
-        return userRepository.save(user);
-    }
-
-    public User getLoggedInUser() {
-        String loggedInUsername = SecurityContextHolder.
-          getContext().getAuthentication().getName();
-        
-        return findByUsername(loggedInUsername);
-    }
 }
--- src/main/java/com/win/twitter/service/UserService.java
+++ src/main/java/com/win/twitter/service/UserService.java
@@ -1,9 +1,16 @@
 package com.win.twitter.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import com.win.twitter.model.Role;
+import com.win.twitter.model.User;
 import com.win.twitter.repository.RoleRepository;
 import com.win.twitter.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +27,31 @@ public class UserService {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return findByUsername(loggedInUsername);
     }
 }
